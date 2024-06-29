944a7d285ee939b89b33d62266599456f614642e
Soucius <soucius.ggwp@gmail.com>
2024-06-29 10:22:01 +0300

    Create user form and validations (data annotations)

--- obj/Debug/net7.0/IdentityApp.AssemblyInfo.cs
+++ obj/Debug/net7.0/IdentityApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IdentityApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4bd6bd81cfd3aa6df26f6dffe65ad4b689ab383")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be1f66eafdde6ea9bb49f6fb51f7a4a177d8821a")]
 [assembly: System.Reflection.AssemblyProductAttribute("IdentityApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IdentityApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
