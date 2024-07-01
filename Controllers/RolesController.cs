da729d4ce7e0a41a1788c1b325d6ea9315534207
Soucius <soucius.ggwp@gmail.com>
2024-07-01 14:03:43 +0300

    Listing roles

--- Controllers/RolesController.cs
+++ Controllers/RolesController.cs
@@ -11,7 +11,7 @@ namespace IdentityApp.Controllers {
         }
 
         public IActionResult Index() {
-            return View();
+            return View(_roleManager.Roles);
         }
 
         [HttpGet]
