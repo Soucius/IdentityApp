aca14c2965a5f06a109630368981b77c0b5a8b1a
Soucius <soucius.ggwp@gmail.com>
2024-07-02 14:36:41 +0300

    giving role to any user

--- obj/Debug/net7.0/IdentityApp.AssemblyInfo.cs
+++ obj/Debug/net7.0/IdentityApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IdentityApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da729d4ce7e0a41a1788c1b325d6ea9315534207")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6028219245ba91bd7727d0d0148220eabe3d5713")]
 [assembly: System.Reflection.AssemblyProductAttribute("IdentityApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IdentityApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
